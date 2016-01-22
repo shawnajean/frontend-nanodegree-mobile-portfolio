dbdd423ad3361f54c8730b7d2ce00d36443c363e
shawnajean <shawnajean@users.noreply.github.com>
2016-01-21 22:48:59 -0600

    Comment out PSI bits in the Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var $ = require('gulp-load-plugins')();
 var del = require('del');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
-var pagespeed = require('psi');
+//var pagespeed = require('psi');
 var reload = browserSync.reload;
 
 // Lint JavaScript
@@ -195,11 +195,11 @@ gulp.task('default', ['clean'], function(cb) {
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
-gulp.task('pagespeed', pagespeed.bind(null, {
+/*gulp.task('pagespeed', pagespeed.bind(null, {
   // By default, we use the PageSpeed Insights
   // free (no API key) tier. You can use a Google
   // Developer API key if you have one. See
   // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
   url: 'https://example.com',
   strategy: 'mobile'
-}));
+}));*/
