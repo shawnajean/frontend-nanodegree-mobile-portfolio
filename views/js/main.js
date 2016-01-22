f7963d3696a9a99081f2b972221e2727920c2f73
shawnajean <shawnajean@users.noreply.github.com>
2016-01-21 21:20:42 -0600

    Revert that last commit

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,8 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var scrollValue = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(( scrollValue / 1250) + (i % 5));
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
