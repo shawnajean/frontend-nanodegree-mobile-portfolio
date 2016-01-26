d05a642c36b63cdfd25968425cff59708622ffe7
shawnajean <shawnajean@users.noreply.github.com>
2016-01-26 12:42:43 -0600

    Create build task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var runSequence = require('run-sequence');
 
 var src = 'src/';
 var dist = 'dist/';
@@ -12,10 +15,10 @@ var paths = {
   js: src + '/**/*.js',
   css: src + '**/*/.css',
   html: src + '**/*.html',
-  jpg: src + '**/*.jpg',
-  png: src + '**/*.png'
+  img: src + '**/*.+(jpg|png)'
 }
 
+//Tester function for learning gulp. :)
 gulp.task('move', ['clean'], function() {
   return gulp.src([paths.css,paths.js,paths.png,paths.jpg]) // Get source files with gulp.src
   // skip this for now .pipe(aGulpPlugin()) // Sends it through a gulp plugin
@@ -26,6 +29,7 @@ gulp.task('move', ['clean'], function() {
 // Clean Output Directory
 gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
+//Minifies JavaScript and CSS files
 gulp.task('minify', function() {
   return gulp.src(paths.html)
     .pipe(useref())
@@ -36,6 +40,19 @@ gulp.task('minify', function() {
 
 // Optimize Images
 gulp.task('images', function() {
-  return gulp.src([paths.png, paths.jpg])
+  return gulp.src(paths.img)
+    .pipe(cache(imagemin()))
     .pipe(gulp.dest('dist/'));
 });
+
+//Clears cached images
+gulp.task('cache:clear', function (done) {
+  return cache.clearAll(done);
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean',
+    ['minify', 'images'],
+    callback
+  );
+})
